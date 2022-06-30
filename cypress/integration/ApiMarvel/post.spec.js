829572976905d52440553338f00e334ed8e41ae2
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-06-30 19:11:07 -0300

    Alterações  gerais

---
+++ cypress/integration/ApiMarvel/post.spec.js
@@ -0,0 +1,29 @@
+
+describe('POST /characters', function(){
+
+    before(function(){
+        cy.back2ThePast()
+        cy.setToken()
+    })
+    
+    it('deve cadastrar um personagem', function() {
+            const character = {
+                name: 'Wanda Maximoff',
+                alias: 'Feiticeira Escarlate',
+                team: ['vingadores'],
+                active: true
+            }
+        cy.request({
+            method: 'POST',
+            url:'/characters',
+            body: character,
+            headers:{ 
+                Authorization: Cypress.env('token')
+            }
+
+        }).then(function(response){
+            expect(response.status).to.eql(201)
+        })
+        
+    });
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,57 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setToken' , function(){
+    
+    cy.request({
+        method: 'POST',
+        url: '/sessions',
+        body: {
+            email:'danilo@gmail.com',
+            password:'qa-cademy'
+        }
+    }).then(function(response){
+        expect(response.status).to.eql(200)
+        cy.log(response.body.token)
+        Cypress.env('token', response.body.token)
+    })
+
+
+})
+
+Cypress.Commands.add('back2ThePast' , function(){
+
+    cy.request({
+        method: 'DELETE',
+        url: '/back2thepast/62b0b4bcfb2a20001619df32',
+       
+    }).then(function(response){
+        expect(response.status).to.eql(200)
+        
+    })
+
+
+})
