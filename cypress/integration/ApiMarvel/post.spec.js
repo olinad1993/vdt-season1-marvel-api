8c06468a7a14fa5cd5ecba86f0d6efa52f560123
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-06-30 21:31:19 -0300

    Validando teste de personagem duplicado

--- cypress/integration/ApiMarvel/post.spec.js
+++ cypress/integration/ApiMarvel/post.spec.js
@@ -19,7 +19,8 @@ describe('POST /characters', function(){
             body: character,
             headers:{ 
                 Authorization: Cypress.env('token')
-            }
+            },
+            failOnStatusCode: false
 
         }).then(function(response){
             expect(response.status).to.eql(201)
@@ -28,4 +29,47 @@ describe('POST /characters', function(){
         })
         
     });
+
+    context.only('Quando o personagem já existe' , function(){
+
+        const character = {
+            name: "Tony Stark",
+            alias: "Homem de Ferro",
+            team: ["Vingadores"],
+            active: true
+    }
+
+        before(function(){
+            cy.api({
+                method: 'POST',
+                url:'/characters',
+                body: character,
+                headers:{ 
+                    Authorization: Cypress.env('token')
+                },
+                failOnStatusCode: false
+    
+                }).then(function(response){
+                    expect(response.status).to.eql(201)
+            })
+        })
+
+        it('Não deve cadastrar duplicado' , function(){
+                cy.api({
+                    method: 'POST',
+                    url:'/characters',
+                    body: character,
+                    headers:{ 
+                        Authorization: Cypress.env('token')
+                    },
+                    failOnStatusCode: false
+        
+                }).then(function(response){
+                    expect(response.status).to.eql(400)
+                    expect(response.body.error).to.equal("Duplicate character")
+                })
+
+        })
+
+    })
 })
